4748e1ba308e3a34b2bc7d5cd5fc247f8ca6e6e4
z3dd <willyzondri@gmail.com>
2021-09-06 20:47:37 +0700

    Refactor Some Code

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,37 @@ const { parallel } = require('gulp');
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
 const pug = require('gulp-pug-3');
+const path = {
+    src: {
+        scss: 'src/scss/*.scss',
+        html: 'src/*.pug'
+    },
+    dist: {
+        css: 'dist/css',
+        html: 'dist'
+    }
+};
 
-gulp.task('scssToCss', async () => {
-    gulp.src('src/scss/*.scss')
+gulp.task('scssToCss', async() => {
+    gulp.src(path.src.scss)
         .pipe(plumber())
+        .pipe(autoprefixer())
         .pipe(sass())
         .pipe(plumber.stop())
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest(path.dist.css))
 })
 
-gulp.task('pugToHtml', async () => {
-    gulp.src('src/*.pug')
+gulp.task('pugToHtml', async() => {
+    gulp.src(path.src.html)
         .pipe(plumber())
         .pipe(pug({
-                doctype: 'html',
-                pretty: true
-            }))
+            doctype: 'html',
+            pretty: true
+        }))
         .pipe(plumber.stop())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest(path.dist.html))
 })
 
 gulp.task('listener', () => {
